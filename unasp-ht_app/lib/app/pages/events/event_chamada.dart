fe4d87244e5e6884f8c86caa6cacbec6f4aa15d8
LuizRicard0 <ricardo.luiz.design@gmail.com>
2020-05-31 20:39:33 -0300

    Scanner de QR Code

--- unasp-ht_app/lib/app/pages/events/event_chamada.dart
+++ unasp-ht_app/lib/app/pages/events/event_chamada.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
 import 'dart:async';
-//import 'package:majascan/majascan.dart';
+import 'package:qrcode_reader/qrcode_reader.dart';
 
 class Chamada extends StatefulWidget {
   @override
@@ -9,81 +8,84 @@ class Chamada extends StatefulWidget {
 }
 
 class _ChamadaState extends State<Chamada> {
-  String scanResult = '';
+  // String scanResult = '';
+  Future<String> _barcodeString;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Leitor Qr Code'.toUpperCase())),
-      body: Column(
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          scanResult == ''
-              ? Text('Result will be displayed here')
-              : Text(scanResult),
-          RaisedButton(
-            color: Colors.deepOrange,
-            child: Text('Scanner'),
-            onPressed: () => scanQRCode,
-          ),
-          Text(
-            scanResult,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          )
-        ],
+      appBar: AppBar(title: Text('Leitor de Qr Code'.toUpperCase())),
+      body: Center(
+          child: FutureBuilder<String>(
+              future: _barcodeString,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                return Text(snapshot.data != null ? snapshot.data : '');
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _barcodeString = QRCodeReader()
+                .setAutoFocusIntervalInMs(200)
+                .setForceAutoFocus(true)
+                .setTorchEnabled(true)
+                .setHandlePermissions(true)
+                .setExecuteAfterPermissionGranted(true)
+                .scan;
+          });
+        },
+        tooltip: 'Reader the QRCode',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
-
-  // Future scanQRCode() async {
-  //   try {
-  //     String qrResult = await MajaScan.startScan(
-  //         title: 'QRcode scanner',
-  //         titleColor: Colors.amberAccent[700],
-  //         qRCornerColor: Colors.orange,
-  //         qRScannerColor: Colors.orange);
-  //     setState(() {
-  //       scanResult = qrResult;
-  //     });
-  //   } on PlatformException catch (ex) {
-  //     if (ex.code == MajaScan.CameraAccessDenied) {
-  //       setState(() {
-  //         scanResult = 'Camera permission was denied';
-  //       });
-  //     } else {
-  //       setState(() {
-  //         scanResult = 'Unknown Error $ex';
-  //       });
-  //     }
-  //   } on FormatException {
-  //     setState(() {
-  //       scanResult = 'You pressed the back button before scanning anything';
-  //     });
-  //   } catch (ex) {
-  //     setState(() {
-  //       scanResult = 'Unknown Error $ex';
-  //     });
-  //   }
-  // }
-
-  Future get scanQRCode async {
-    final cameraScanResult = await scanner.scan();
-    setState(() {
-      scanResult = cameraScanResult;
-    });
-  }
-
-  // Future scanQrCode() async {
-  //   try {
-  //     final result = await BarcodeScanner.scan();
-
-  //     setState(() {
-  //       code = result as String;
-  //     });
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
-
 }
+
+// Future scanQRCode() async {
+//   try {
+//     String qrResult = await MajaScan.startScan(
+//         title: 'QRcode scanner',
+//         titleColor: Colors.amberAccent[700],
+//         qRCornerColor: Colors.orange,
+//         qRScannerColor: Colors.orange);
+//     setState(() {
+//       scanResult = qrResult;
+//     });
+//   } on PlatformException catch (ex) {
+//     if (ex.code == MajaScan.CameraAccessDenied) {
+//       setState(() {
+//         scanResult = 'Camera permission was denied';
+//       });
+//     } else {
+//       setState(() {
+//         scanResult = 'Unknown Error $ex';
+//       });
+//     }
+//   } on FormatException {
+//     setState(() {
+//       scanResult = 'You pressed the back button before scanning anything';
+//     });
+//   } catch (ex) {
+//     setState(() {
+//       scanResult = 'Unknown Error $ex';
+//     });
+//   }
+// }
+
+// Future get scanQRCode async {
+//   final cameraScanResult = await scanner.scan();
+//   setState(() {
+//     scanResult = cameraScanResult;
+//   });
+// }
+
+// Future scanQrCode() async {
+//   try {
+//     final result = await BarcodeScanner.scan();
+
+//     setState(() {
+//       code = result as String;
+//     });
+//   } catch (e) {
+//     print(e);
+//   }
+// }
